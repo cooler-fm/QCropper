6ef01e86c7c3fc83071a1b12551d88946f2631b1
smoothfm <131091215+smoothfm@users.noreply.github.com>
2023-10-25 11:42:13 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,25 +1,19 @@
-// swift-tools-version: 5.9
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "QCropper",
-		platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "QCropper",
             targets: ["QCropper"]),
     ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "QCropper",
-            path: "QCropper"),
-        .testTarget(
-            name: "QCropperTests",
-            dependencies: ["QCropper"]),
+            dependencies: [],
+            path: "QCropper")
     ]
 )
